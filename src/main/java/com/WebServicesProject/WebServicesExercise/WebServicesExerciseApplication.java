b3bfc8f4bcf52f05ed32e9720b3d260fb82ea236
Mikel Kota <mikelkota@gmail.com>
2021-07-17 00:05:45 -0400

    Skelaton Java Spring Boot Application generated via Spring Boot Initializr (https://start.spring.io/) (Includes Jersey framework dependencies.)

---
+++ src/main/java/com/WebServicesProject/WebServicesExercise/WebServicesExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.WebServicesProject.WebServicesExercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServicesExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServicesExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/WebServicesProject/WebServicesExercise/WebServicesExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WebServicesProject.WebServicesExercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServicesExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
